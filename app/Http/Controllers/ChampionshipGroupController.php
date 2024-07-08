0f3de168c5dd35c2a5a6f2e137a608400ba2b80b
André Fontoura <anddfontoura91@gmail.com>
2024-07-08 19:43:04 -0300

    Auth + models e controllers

---
+++ app/Http/Controllers/ChampionshipGroupController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChampionshipGroup;
+use Illuminate\Http\Request;
+
+class ChampionshipGroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ChampionshipGroup $championshipGroup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ChampionshipGroup $championshipGroup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ChampionshipGroup $championshipGroup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ChampionshipGroup $championshipGroup)
+    {
+        //
+    }
+}
